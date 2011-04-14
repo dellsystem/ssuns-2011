d8140c6477c2700330f7c5a207b8e9902c03b892
dellsystem <ilostwaldo@gmail.com>
2011-04-14 19:05:51 -0400

    forget what i was doing

--- board/cache/data_cfg_theme_prosilver.php
+++
@@ -1,4 +0,0 @@
-<?php exit; ?>
-1330651647
-161
-a:5:{s:4:"name";s:9:"prosilver";s:9:"copyright";s:24:"&copy; phpBB Group, 2007";s:7:"version";s:5:"3.0.8";s:14:"parse_css_file";b:1;s:8:"filetime";i:1290169356;}
--- board/cache/tpl_prosilver-se_template_body.html.php
+++
@@ -1,7 +0,0 @@
-<?php if (!defined('IN_PHPBB')) exit; $this->_tpl_include('overall_header.html'); ?>
-
-
-<?php echo (isset($this->_rootref['PAGE_CONTENT'])) ? $this->_rootref['PAGE_CONTENT'] : ''; ?>
-
-
-<?php $this->_tpl_include('overall_footer.html'); ?>
