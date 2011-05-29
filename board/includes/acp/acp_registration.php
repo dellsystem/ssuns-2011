fc54de230722ae8dda9fc2091543690b8f45d126
dellsystem <ilostwaldo@gmail.com>
2011-05-28 20:05:51 -0400

    add list of schools to registration module

--- board/includes/acp/acp_registration.php
+++ board/includes/acp/acp_registration.php
@@ -30,6 +30,26 @@ class acp_registration {
 			case 'overview':
 				$this->page_title = 'SSUNS registration management';
 				$this->tpl_name = 'acp_registration';
+				
+				// Shows you all the schools
+				$sql = "SELECT school_id, school_name, country, registration_time, number_of_delegates
+						FROM " . SCHOOLS_CONTACT_TABLE . "
+						ORDER BY registration_time DESC";
+				$result = $db->sql_query($sql);
+				
+				while ($row = $db->sql_fetchrow($result)) {
+					$template->assign_block_vars('schools', array(
+						'ID'					=> $row['school_id'],
+						'NAME'					=> $row['school_name'],
+						'COUNTRY'				=> $row['country'],
+						'REGISTRATION_TIME'		=> $user->format_date($row['registration_time']),
+						'NUMBER_OF_DELEGATES'	=> $row['number_of_delegates'],
+						'IS_APPROVED'			=> $row['is_approved'],
+						'U_EDIT'				=> $this->u_action . '&amp;edit=' . $row['school_id'],
+						'U_DELETE'				=> $this->u_action . '&amp;delete=' . $row['school_id'],
+						'U_APPROVE'				=> $this->u_action . '&amp;approve=' . $row['school_id'])
+					);
+				}
             break;
       	}
 	}
